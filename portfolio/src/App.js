bfb5f6e6f58b5f9b827ca5c0f91b6a9867a9a259
Wesley Grubbs <wes@pitchinteractive.com>
2023-09-23 23:00:59 -0700

    Update App.js
    Just making some basic mods to test.

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Wesley Grubbs' future website.
         </p>
         <a
           className="App-link"
