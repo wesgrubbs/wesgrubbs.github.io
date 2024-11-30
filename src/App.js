13778a675151f4cad8c467c312dd43aa2cb7e514
Wesley Grubbs <wes@pitchinteractive.com>
2024-11-29 17:10:15 -0800

    Pushing updates
    Getting gh pages working with deployment. One more push to verify changes are taking effect.

--- src/App.js
+++ src/App.js
@@ -5,8 +5,15 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Wesley Grubbs</p>
+        <p>
+          <b>Wesley Grubbs</b> is a multidisciplinary creator blending the
+          worlds of code and design to craft unique, immersive experiences. With
+          a skillset that spans data visualization, interface design, and iOS
+          sports applications, Wesley seamlessly weaves functionality and
+          aesthetics to bring ideas to life. Whether developing dynamic digital
+          tools or designing intuitive user interfaces, his work is driven by a
+          passion for merging technical precision with creative expression.
+        </p>
       </header>
     </div>
   );
