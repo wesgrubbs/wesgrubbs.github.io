13037cc4a7600309a3fb7415b36202b7cdf9c50b
Wesley Grubbs <wes@pitchinteractive.com>
2025-02-23 01:11:36 -0800

    addressing site not loading on gh pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/",
+  base: "./",
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      },
+    },
+  },
 });
