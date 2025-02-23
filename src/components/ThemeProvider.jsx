935618ed46ba3d4c4688d7a5aafef99cf091925b
Wesley Grubbs <wes@pitchinteractive.com>
2025-02-23 01:00:28 -0800

    Setting up for github pages
    Wasn't working. Now it works.

--- src/components/ThemeProvider.jsx
+++ src/components/ThemeProvider.jsx
@@ -3,6 +3,7 @@ import PropTypes from "prop-types";
 
 const ThemeContext = createContext();
 
+// eslint-disable-next-line react-refresh/only-export-components
 export const useTheme = () => {
   const context = useContext(ThemeContext);
   if (!context) {
